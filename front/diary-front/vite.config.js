e2695588ea562b5d7116628fa31b1466141f3115
Gyuna Kim <kkn00077@gmail.com>
2024-12-18 17:35:47 +0900

    svg 플러그인 설정 추가

--- front/diary-front/vite.config.js
+++ front/diary-front/vite.config.js
@@ -4,17 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    vueJsx()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [vue(), vueDevTools(), vueJsx(), svgLoader()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
