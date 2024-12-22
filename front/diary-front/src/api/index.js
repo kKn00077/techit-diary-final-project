84ebfb4e63271ff5f2ec064dbe78e3ca2da290c8
Gyuna Kim <kkn00077@gmail.com>
2024-12-23 08:51:29 +0900

    쿠키 자동 저장을 위한 Vue axios 세팅 추가

--- front/diary-front/src/api/index.js
+++ front/diary-front/src/api/index.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const api = axios.create({
 	baseURL: 'http://localhost:5000/', // 기본 URL
 	timeout: 5000, // 요청 타임아웃
+	withCredentials: true, // 쿠키를 자동으로 포함하고 저장
 	headers: {
 		'Content-Type': 'application/json'
 	}
